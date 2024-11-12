d52642240a7a93e5f504f0671827d12842e2fc67
smj616 <sara.mar.j.616@gmail.com>
2024-11-12 10:50:34 +0100

    createDatabase updated, classes to access the data created, method to generate a list by filters created

---
+++ src/com/models/Camion.java
@@ -0,0 +1,9 @@
+package com.models;
+
+public class Camion extends Vehiculo {
+    public Camion(String matricula, String marca, String modelo, String carroceria, String combustible, 
+                  double consumo, double plazas, double kilometros, double precioCompra, boolean alquilado) {
+        super(matricula, "camion", marca, modelo, carroceria, combustible, consumo, plazas, kilometros, precioCompra, alquilado);
+    }
+}
+
--- src/com/models/Coche.java
+++ src/com/models/Coche.java
@@ -1,5 +1,9 @@
 package com.models;
 
-public class Coche {
-
+public class Coche extends Vehiculo {
+    public Coche(String matricula, String marca, String modelo, String carroceria, String combustible, 
+                 double consumo, double plazas, double kilometros, double precioCompra, boolean alquilado) {
+        super(matricula, "coche", marca, modelo, carroceria, combustible, consumo, plazas, kilometros, precioCompra, alquilado);
+    }
 }
+
