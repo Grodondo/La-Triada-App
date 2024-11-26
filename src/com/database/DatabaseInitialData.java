360107324913e8de6326ffd092e0fa33163e3b9c
smj616 <sara.mar.j.616@gmail.com>
2024-11-26 09:01:51 +0100

    Added new vehicles and added method to get availability

--- src/com/database/DatabaseInitialData.java
+++ src/com/database/DatabaseInitialData.java
@@ -6,21 +6,25 @@ import java.sql.SQLException;
 import com.database.createDatabase;
 import com.models.Vehiculo;
 
-
 /**
  * Clase encargada de inicializar la base de datos para la aplicación.
  * 
- * Contiene métodos para crear tablas, triggers y datos iniciales en la base de datos SQLite.
- * Esta clase utiliza los métodos definidos en la clase {@link createDatabase} para establecer
- * la estructura inicial y poblarla con datos predeterminados.
+ * Contiene métodos para crear tablas, triggers y datos iniciales en la base de
+ * datos SQLite. Esta clase utiliza los métodos definidos en la clase
+ * {@link createDatabase} para establecer la estructura inicial y poblarla con
+ * datos predeterminados.
  * 
  * <p>
  * El flujo principal incluye:
  * <ul>
- *   <li>Creación de la tabla de vehículos ({@link createDatabase#createTableVehiculos()})</li>
- *   <li>Creación de la tabla de vehículos alquilados ({@link createDatabase#createTableAlquilados()})</li>
- *   <li>Creación de un trigger para manejar el retorno de vehículos ({@link createDatabase#createReturnTrigger()})</li>
- *   <li>Inserción de datos iniciales en la tabla de vehículos ({@link #insertarDatosIniciales()})</li>
+ * <li>Creación de la tabla de vehículos
+ * ({@link createDatabase#createTableVehiculos()})</li>
+ * <li>Creación de la tabla de vehículos alquilados
+ * ({@link createDatabase#createTableAlquilados()})</li>
+ * <li>Creación de un trigger para manejar el retorno de vehículos
+ * ({@link createDatabase#createReturnTrigger()})</li>
+ * <li>Inserción de datos iniciales en la tabla de vehículos
+ * ({@link #insertarDatosIniciales()})</li>
  * </ul>
  * </p>
  * 
@@ -31,10 +35,10 @@ import com.models.Vehiculo;
 public class DatabaseInitialData {
 	public static void main(String[] args) {
 		// Usados solo para correcciones
-		/* 
-		createDatabase.EliminarTabla("alquilado");
-		createDatabase.EliminarTabla("vehiculo"); 
-		*/
+		/*
+		 * createDatabase.EliminarTabla("alquilado");
+		 * createDatabase.EliminarTabla("vehiculo");
+		 */
 
 		createDatabase.createTableVehiculos();
 		createDatabase.createTableAlquilados();
@@ -59,32 +63,59 @@ public class DatabaseInitialData {
 	public static void insertarDatosIniciales() {
 		try {
 			// Insertar coches
-			Vehiculo.insertarVehiculoInicial("7654DCY", "coche", "toyota", "corolla sedan", "berlina", "hibrido", 5.00, 5,
-					45678, 15000.00, 20.00, false);
-			Vehiculo.insertarVehiculoInicial("9238JRC", "coche", "toyota", "c-hr", "suv", "hibrido", 4.70, 5, 98734, 18900.00,
-					23.90, false);
-			Vehiculo.insertarVehiculoInicial("6789LTR", "coche", "toyota", "yaris", "turismo", "hibrido", 4.60, 5, 20345,
-					20000.00, 25.00, false);
+			Vehiculo.insertarVehiculoInicial("7654DCY", "coche", "toyota", "corolla sedan", "berlina", "hibrido", 5.00,
+					5, 45678, 15000.00, 20.00, false);
+			Vehiculo.insertarVehiculoInicial("9238JRC", "coche", "toyota", "c-hr", "suv", "hibrido", 4.70, 5, 98734,
+					18900.00, 23.90, false);
+			Vehiculo.insertarVehiculoInicial("6789LTR", "coche", "toyota", "yaris", "turismo", "hibrido", 4.60, 5,
+					20345, 20000.00, 25.00, false);
 			Vehiculo.insertarVehiculoInicial("0987KPT", "coche", "kia", "xceed", "crossover", "hibrido", 4.95, 5, 18928,
 					19000.00, 24.00, false);
 			Vehiculo.insertarVehiculoInicial("2348HTZ", "coche", "hyundai", "i20", "turismo", "hibrido", 5.15, 5, 38762,
 					18000.00, 23.00, false);
-
+			// Nuevos
+			Vehiculo.insertarVehiculoInicial("1234ABC", "coche", "ford", "focus hatchback", "compacto", "gasolina",
+					4.50, 5, 45679, 12000.00, 18.00, true);
+			Vehiculo.insertarVehiculoInicial("2345BCD", "coche", "honda", "civic sedan", "berlina", "diesel", 4.80, 5,
+					45680, 18000.00, 25.00, false);
+			Vehiculo.insertarVehiculoInicial("3456CDE", "coche", "audi", "a3 sportback", "compacto", "hibrido", 5.20, 5,
+					45681, 25000.00, 22.00, true);
+			Vehiculo.insertarVehiculoInicial("4567DEF", "coche", "mercedes", "benz clase c", "berlina", "gasolina",
+					5.30, 5, 45682, 30000.00, 28.00, false);
+			Vehiculo.insertarVehiculoInicial("5678EFG", "coche", "bmw", "serie 3 sedan", "berlina", "diesel", 5.10, 5,
+					45683, 35000.00, 30.00, true);
+			Vehiculo.insertarVehiculoInicial("6789FGH", "coche", "renault", "megane hatchback", "compacto", "gasolina",
+					4.60, 5, 45684, 10000.00, 17.00, false);
 			// Insertar motos
-			Vehiculo.insertarVehiculoInicial("7539JBR", "moto", "bmw", "r 1300 gs", "tracker", "gasolina", 5.00, null, 14556,
-					15000.00, 20.00, false);
-			Vehiculo.insertarVehiculoInicial("8743GVT", "moto", "kawasaki", "ninja 400", "sport", "gasolina", 4.90, null,
-					19384, 14000.00, 19.00, false);
-			Vehiculo.insertarVehiculoInicial("9874HRC", "moto", "yamaha", "xsr125 legacy", "heritage", "gasolina", 4.57, null,
-					12345, 13900.00, 18.00, false);
-
+			Vehiculo.insertarVehiculoInicial("7539JBR", "moto", "bmw", "r 1300 gs", "tracker", "gasolina", 5.00, null,
+					14556, 15000.00, 20.00, false);
+			Vehiculo.insertarVehiculoInicial("8743GVT", "moto", "kawasaki", "ninja 400", "sport", "gasolina", 4.90,
+					null, 19384, 14000.00, 19.00, false);
+			Vehiculo.insertarVehiculoInicial("9874HRC", "moto", "yamaha", "xsr125 legacy", "heritage", "gasolina", 4.57,
+					null, 12345, 13900.00, 18.00, false);
+			// Nuevas
+			Vehiculo.insertarVehiculoInicial("8437KLC", "moto", "harley davidson", "iron 883", "chopper", "gasolina",
+					4.80, null, 14557, 12000.00, 18.00, true);
+			Vehiculo.insertarVehiculoInicial("9472LKM", "moto", "yamaha", "mt-09", "naked", "gasolina", 4.90, null,
+					14558, 11000.00, 16.00, false);
+			Vehiculo.insertarVehiculoInicial("8365MNP", "moto", "ducati", "monster 1200", "naked", "gasolina", 5.10,
+					null, 14559, 16000.00, 22.00, true);
+			Vehiculo.insertarVehiculoInicial("9203OPQ", "moto", "kawasaki", "ninja zx-6r", "deportiva", "gasolina",
+					5.20, null, 14560, 14000.00, 19.00, false);
 			// Insertar camiones
-			Vehiculo.insertarVehiculoInicial("8742GTR", "camion", "mercedes", "atego 1418", "ligero", "diesel", 6.34, 3, 23456,
-					19000.00, 24.00, false);
-			Vehiculo.insertarVehiculoInicial("7934LTC", "camion", "iveco", "stralis ad", "pesado", "diesel", 5.79, 3, 45678,
-					18000.00, 23.00, false);
-			Vehiculo.insertarVehiculoInicial("9876KCT", "camion", "volvo", "fh16 750", "pesado", "diesel", 5.95, 3, 34563,
-					15000.00, 20.00, false);
+			Vehiculo.insertarVehiculoInicial("8742GTR", "camion", "mercedes", "atego 1418", "ligero", "diesel", 6.34, 3,
+					23456, 19000.00, 24.00, false);
+			Vehiculo.insertarVehiculoInicial("7934LTC", "camion", "iveco", "stralis ad", "pesado", "diesel", 5.79, 3,
+					45678, 18000.00, 23.00, false);
+			Vehiculo.insertarVehiculoInicial("9876KCT", "camion", "volvo", "fh16 750", "pesado", "diesel", 5.95, 3,
+					34563, 15000.00, 20.00, false);
+			// Nuevos
+			Vehiculo.insertarVehiculoInicial("9234HTL", "camion", "volvo", "fh 12", "pesado", "diesel", 7.50, 4, 23457,
+					25000.00, 30.00, true);
+			Vehiculo.insertarVehiculoInicial("8321KRM", "camion", "man", "tgs 18.400", "ligero", "diesel", 6.80, 2,
+					23458, 22000.00, 28.00, false);
+			Vehiculo.insertarVehiculoInicial("7562LKT", "camion", "scania", "r 450", "pesado", "diesel", 7.20, 3, 23459,
+					27000.00, 26.00, true);
 
 			System.out.println("Datos iniciales insertados correctamente.");
 		} catch (Exception e) {
--- src/com/models/Vehiculo.java
+++ src/com/models/Vehiculo.java
@@ -7,7 +7,9 @@ import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Types;
 
 /**
@@ -83,15 +85,15 @@ public class Vehiculo {
 	}
 
 	// Metodo para insertar los primeros datos
-	public static void insertarVehiculoInicial(String matricula, String tipo, String marca, String modelo, String carroceria,
-			String combustible, double consumo, Integer plazas, double kilometros, double precioCompra,
-			double precioAlquiler, boolean alquilado) {
-			// Directorio base para las imágenes
+	public static void insertarVehiculoInicial(String matricula, String tipo, String marca, String modelo,
+			String carroceria, String combustible, double consumo, Integer plazas, double kilometros,
+			double precioCompra, double precioAlquiler, boolean alquilado) {
+		// Directorio base para las imágenes
 		String directorioImagenes = AppConfig.PROJECT_PATH + "\\src\\com\\database\\imagenes\\";
 
 		// Generar la ruta de la imagen usando la matrícula
 		String rutaImagen = Paths.get(directorioImagenes, matricula + ".jpg").toString();
-			
+
 		String sql = "INSERT INTO vehiculo(matricula, tipo, marca, modelo, carroceria, combustible, consumo, plazas, kilometros, precio_compra, precio_venta, precio_alquiler, alquilado, imagen) "
 				+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
@@ -105,7 +107,7 @@ public class Vehiculo {
 			pstmt.setDouble(7, consumo);
 			if (plazas != null) {
 				pstmt.setInt(8, plazas);
-			}else {
+			} else {
 				pstmt.setNull(8, Types.INTEGER);
 			}
 			pstmt.setDouble(9, kilometros);
@@ -116,46 +118,70 @@ public class Vehiculo {
 			pstmt.setString(14, rutaImagen); // Ruta generada de la imagen
 
 			System.out.printf(
-				    "Inserting: matricula=%s, tipo=%s, marca=%s, modelo=%s, carroceria=%s, combustible=%s, consumo=%.2f, plazas=%s, kilometros=%.2f, precioCompra=%.2f, precioVenta=%.2f, precioAlquiler=%.2f, alquilado=%b, imagen=%s%n",
-				    matricula, tipo, marca, modelo, carroceria, combustible, consumo, plazas, kilometros, precioCompra,
-				    precioCompra * 1.33, precioAlquiler, alquilado, rutaImagen);
-			
+					"Inserting: matricula=%s, tipo=%s, marca=%s, modelo=%s, carroceria=%s, combustible=%s, consumo=%.2f, plazas=%s, kilometros=%.2f, precioCompra=%.2f, precioVenta=%.2f, precioAlquiler=%.2f, alquilado=%b, imagen=%s%n",
+					matricula, tipo, marca, modelo, carroceria, combustible, consumo, plazas, kilometros, precioCompra,
+					precioCompra * 1.33, precioAlquiler, alquilado, rutaImagen);
+
 			pstmt.executeUpdate();
 			System.out.println("Vehículo insertado correctamente: " + matricula);
 		} catch (SQLException e) {
 			System.out.println("Error al insertar el vehículo: " + e.getMessage());
 		}
 	}
-	
+
+	public static int disponibilidadVehiculos(String tipo) {
+		String sql = "SELECT COUNT(tipo) FROM vehiculo WHERE tipo = " + tipo;
+		int cantidad = -1; // Valor predeterminado en caso de error
+		try (Connection conn = createDatabase.connect(); Statement stmt = conn.createStatement()) {
+			// Ejecutar la consulta y obtener el resultado
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// Si hay resultados, obtener el conteo
+			if (rs.next()) {
+				cantidad = rs.getInt(1); // El primer (y único) valor de la consulta es el conteo
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage()); // Mostrar el error en caso de excepciones
+		}
+		return cantidad; // Retornar la cantidad obtenida
+	}
+
 	/**
 	 * Inserta un nuevo vehículo en la base de datos con los datos proporcionados.
-	 * Además, genera la ruta de la imagen asociada al vehículo utilizando la matrícula 
-	 * y la guarda en el campo correspondiente de la base de datos.
+	 * Además, genera la ruta de la imagen asociada al vehículo utilizando la
+	 * matrícula y la guarda en el campo correspondiente de la base de datos.
 	 * 
-	 * @param matricula La matrícula del vehículo. 
-	 * @param tipo El tipo de vehículo (por ejemplo, coche, moto, etc.).
-	 * @param marca La marca del vehículo.
-	 * @param modelo El modelo del vehículo.
-	 * @param carroceria El tipo de carrocería del vehículo (por ejemplo, berlina, SUV, etc.).
-	 * @param combustible El tipo de combustible utilizado por el vehículo (por ejemplo, gasolina, diésel, eléctrico, etc.).
-	 * @param consumo El consumo de combustible del vehículo (en litros por 100 km).
-	 * @param plazas El número de plazas disponibles en el vehículo.
-	 * @param kilometros Los kilómetros recorridos por el vehículo.
-	 * @param precioCompra El precio de compra del vehículo.
-	 * @param precioAlquiler El precio de alquiler del vehículo por unidad de tiempo.
-	 * @param alquilado Indica si el vehículo está alquilado (true si está alquilado, false si no lo está).
+	 * @param matricula      La matrícula del vehículo.
+	 * @param tipo           El tipo de vehículo (por ejemplo, coche, moto, etc.).
+	 * @param marca          La marca del vehículo.
+	 * @param modelo         El modelo del vehículo.
+	 * @param carroceria     El tipo de carrocería del vehículo (por ejemplo,
+	 *                       berlina, SUV, etc.).
+	 * @param combustible    El tipo de combustible utilizado por el vehículo (por
+	 *                       ejemplo, gasolina, diésel, eléctrico, etc.).
+	 * @param consumo        El consumo de combustible del vehículo (en litros por
+	 *                       100 km).
+	 * @param plazas         El número de plazas disponibles en el vehículo.
+	 * @param kilometros     Los kilómetros recorridos por el vehículo.
+	 * @param precioCompra   El precio de compra del vehículo.
+	 * @param precioAlquiler El precio de alquiler del vehículo por unidad de
+	 *                       tiempo.
+	 * @param alquilado      Indica si el vehículo está alquilado (true si está
+	 *                       alquilado, false si no lo está).
 	 * 
-	 * @throws SQLException Si ocurre algún error al interactuar con la base de datos.
-	 * @throws Exception Si ocurre algún error general durante la inserción.
+	 * @throws SQLException Si ocurre algún error al interactuar con la base de
+	 *                      datos.
+	 * @throws Exception    Si ocurre algún error general durante la inserción.
 	 */
 	public static void insertarVehiculo(String matricula, String tipo, String marca, String modelo, String carroceria,
-			String combustible, double consumo, Integer plazas, double kilometros, double precioCompra, boolean alquilado) {
-			// Directorio base para las imágenes
+			String combustible, double consumo, Integer plazas, double kilometros, double precioCompra,
+			boolean alquilado) {
+		// Directorio base para las imágenes
 		String directorioImagenes = "./La-Triada-App/src/com/database/imagenes/";
 
 		// Generar la ruta de la imagen usando la matrícula
 		String rutaImagen = Paths.get(directorioImagenes, matricula + ".jpg").toString();
-			
+
 		String sql = "INSERT INTO vehiculo(matricula, tipo, marca, modelo, carroceria, combustible, consumo, plazas, kilometros, precio_compra, precio_venta, precio_alquiler, alquilado, imagen) "
 				+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
@@ -169,13 +195,13 @@ public class Vehiculo {
 			pstmt.setDouble(7, consumo);
 			if (plazas != null) {
 				pstmt.setInt(8, plazas);
-			}else {
+			} else {
 				pstmt.setNull(8, Types.INTEGER);
 			}
 			pstmt.setDouble(9, kilometros);
 			pstmt.setDouble(10, precioCompra);
 			pstmt.setDouble(11, precioCompra * 1.33); // Ejemplo: 33% margen sobre el precio de compra
-			pstmt.setDouble(12, (precioCompra * 1.33)/1000.00);
+			pstmt.setDouble(12, (precioCompra * 1.33) / 1000.00);
 			pstmt.setBoolean(13, alquilado);
 			pstmt.setString(14, rutaImagen); // Ruta generada de la imagen
 
@@ -255,8 +281,7 @@ public class Vehiculo {
 	public String getRutaImagen() {
 		return imagen;
 	}
-	
-	
+
 	/**
 	 * Muestra los datos del vehículo en la consola.
 	 */
@@ -277,5 +302,5 @@ public class Vehiculo {
 		System.out.println("Ruta de la imagen: " + imagen);
 		System.out.println("-------------------------------------------------");
 	}
-	
+
 }
